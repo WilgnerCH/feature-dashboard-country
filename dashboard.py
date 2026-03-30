ffdf49ca84f51e3776aefa226576685ee1be7a47
Wilgner Chayder Medina Santos <136656185+WilgnerCH@users.noreply.github.com>
2026-03-30 18:16:55 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,32 +3,18 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-st.title("Canada Trade Dashboard 🇨🇦")
+st.title("🇧🇷 Brazil ↔ Canada Trade Dashboard")
 
 @st.cache_data
 def load_data():
-    url = "https://huggingface.co/datasets/WilgnerCH/canada-trade-data/resolve/main/canada_trade_full.csv.gz"
-    return pd.read_csv(url)
+    return pd.read_csv("brazil_trade_summary.csv")
 
 
 df = load_data()
 
-# processar leve no front (temporário)
-df["Value"] = pd.to_numeric(df["Value"], errors="coerce")
-df = df.dropna(subset=["Value"])
-
-df_country = (
-    df.groupby(["date", "Country", "trade_type"])["Value"]
-    .sum()
-    .reset_index()
-)
-
-# filtro
-country = st.selectbox("Select Country", df_country["Country"].unique())
-
-filtered = df_country[df_country["Country"] == country]
-
 # pivot para gráfico
-pivot = filtered.pivot(index="date", columns="trade_type", values="Value")
+pivot = df.pivot(index="date", columns="trade_type", values="Value")
+
+st.subheader("Import vs Export")
 
 st.line_chart(pivot)
