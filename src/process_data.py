f830591011aefb1531464fbcb8780dd351494db6
Wilgner Chayder Medina Santos <136656185+WilgnerCH@users.noreply.github.com>
2026-03-30 18:47:54 -0400

    Update process_data.py

--- src/process_data.py
+++ src/process_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 def process_data(df):
     print("Final processing...")
 
-    # Converter date (vem como Period)
+    # Converter date
     df["date"] = df["date"].astype(str)
     df["date"] = pd.to_datetime(df["date"])
 
