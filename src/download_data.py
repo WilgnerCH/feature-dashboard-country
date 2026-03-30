8d1ac0d6ac12fb8ef83c85ec7afb35bc879825f1
Wilgner Chayder Medina Santos <136656185+WilgnerCH@users.noreply.github.com>
2026-03-30 16:24:37 -0400

    Update download_data.py

--- src/download_data.py
+++ src/download_data.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+
+def download_data():
+
+    print("Downloading data from Hugging Face...")
+
+    url = "https://huggingface.co/datasets/WilgnerCH/canada-trade-data/resolve/main/canada_trade_full.csv.gz"
+
+    df = pd.read_csv(url)
+
+    print("Data loaded:", len(df))
+
+    return df
