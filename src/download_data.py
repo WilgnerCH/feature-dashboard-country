b2e797ad0045cbdecb1f92b142d8cb718e0ae969
Wilgner Chayder Medina Santos <136656185+WilgnerCH@users.noreply.github.com>
2026-03-30 18:15:30 -0400

    Update download_data.py

--- src/download_data.py
+++ src/download_data.py
@@ -3,12 +3,18 @@ import pandas as pd
 
 def download_data():
 
-    print("Downloading data from Hugging Face...")
+    print("Downloading dataset...")
 
     url = "https://huggingface.co/datasets/WilgnerCH/canada-trade-data/resolve/main/canada_trade_full.csv.gz"
 
-    df = pd.read_csv(url)
+    # limitar leitura
+    df = pd.read_csv(
+        url,
+        usecols=["date", "Country", "trade_type", "Value"],
+        dtype={"Country": "string", "trade_type": "string"},
+        low_memory=False
+    )
 
-    print("Data loaded:", len(df))
+    print("Rows loaded:", len(df))
 
     return df
