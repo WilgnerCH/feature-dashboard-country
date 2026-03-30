2a777c3cfc9ceee1b4cc1f1af3a5004f6a6abd03
Wilgner Chayder Medina Santos <136656185+WilgnerCH@users.noreply.github.com>
2026-03-30 18:38:30 -0400

    Update download_data.py

--- src/download_data.py
+++ src/download_data.py
@@ -1,18 +1,16 @@
 import pandas as pd
 
-
 def download_data():
-
     print("Downloading dataset...")
 
     url = "https://huggingface.co/datasets/WilgnerCH/canada-trade-data/resolve/main/canada_trade_full.csv.gz"
 
-    # limitar leitura
     df = pd.read_csv(
         url,
         usecols=["date", "Country", "trade_type", "Value"],
         dtype={"Country": "string", "trade_type": "string"},
-        low_memory=False
+        low_memory=False,
+        nrows=500_000  # LIMITADOR
     )
 
     print("Rows loaded:", len(df))
