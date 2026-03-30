63bdb0aefa9b4220571d6caaad6ac2f02b931cee
Wilgner Chayder Medina Santos <136656185+WilgnerCH@users.noreply.github.com>
2026-03-30 18:16:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from src.download_data import download_data
+from src.process_data import process_data
 
+
+def main():
+
+    print("Starting pipeline...")
+
+    df = download_data()
+
+    df_brazil = process_data(df)
+
+    # salvar arquivo leve
+    df_brazil.to_csv("brazil_trade_summary.csv", index=False)
+
+    print("Dataset created: brazil_trade_summary.csv")
+
+
+if __name__ == "__main__":
+    main()
